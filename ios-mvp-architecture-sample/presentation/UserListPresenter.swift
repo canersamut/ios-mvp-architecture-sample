8327c44108500f250da8586a4a9a1154efe1d070
Caner SAMUT <canersamut19@gmail.com>
2018-12-28 10:53:22 +0300

    First commit

---
+++ ios-mvp-architecture-sample/presentation/UserListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  UserListPresenter.swift
+//  ios-mvp-architecture-sample
+//
+//  Created by inofab-caner on 28.12.2018.
+//  Copyright © 2018 inofab-caner. All rights reserved.
+//
+
+import Foundation
+
+class UserListPresenter {
+    
+}
