7dda7dde792350ac91cd4b0a9587844e3cbfdc06
Caner SAMUT <canersamut19@gmail.com>
2018-12-28 10:42:47 +0300

    Initial Commit

---
+++ ios-mvp-architecture-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-mvp-architecture-sample
+//
+//  Created by inofab-caner on 28.12.2018.
+//  Copyright © 2018 inofab-caner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios-mvp-architecture-sampleTests/ios_mvp_architecture_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_mvp_architecture_sampleTests.swift
+//  ios-mvp-architecture-sampleTests
+//
+//  Created by inofab-caner on 28.12.2018.
+//  Copyright © 2018 inofab-caner. All rights reserved.
+//
+
+import XCTest
+@testable import ios_mvp_architecture_sample
+
+class ios_mvp_architecture_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
